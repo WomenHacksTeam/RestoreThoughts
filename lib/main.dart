5bdefc7ff25c16abe2baa094ec2e532b6699705e
Vikasni Kalahasthi <vikasnikalahasthi@Vikasnis-MBP.attlocal.net>
2021-04-10 23:02:10 -0700

    Added Package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/screens/note_list.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() {
   runApp(MyApp());
